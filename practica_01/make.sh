74eaa47551c20700ff8236ff7fbdb50d997c5925
andugu <andugu@users.noreply.github.com>
2020-03-29 20:52:01 +0200

    Final touch 🍳

--- practica_01/make.sh
+++ practica_01/make.sh
@@ -9,9 +9,10 @@ echo "##############################"
 echo ""
 echo "Please select an option:"
 echo ""
-echo "1. Compile project and docs & make .jar executabl & docs pdf"
-echo "2. Remove all *.class and *.jar files from /src and pdf docs"
-echo "3. Remove all *.class and *.jar files from /scr and keep pdf docs"
+echo "1. Compile project and docs & make .jar executabl"
+echo "2. Make docs"
+echo "3. Remove all *.class and *.jar files"
+echo "4. Remove docs.pdf"
 echo ""
 echo "I choose:"
 echo ""
@@ -21,23 +22,21 @@ echo ""
 if [ $n == 1 ]; then
 	javac -cp $CLASSPATH src/*.java
 	jar cfm practica_01.jar Manifest.txt src/*.class
+fi
 
-	echo ""
-
+if [ $n == 2 ]; then
 	pdflatex -output-directory=./docs ./docs/docs.tex
 	rm ./docs/*.aux
 	rm ./docs/*.log
 fi
 
-if [ $n == 2 ]; then
-	rm -rf ./src/*.class
-	rm -rf ./*.jar
-	rm -rf ./docs/documentació.pdf
-fi
-
 if [ $n == 3 ]; then
 	rm -rf ./src/*.class
 	rm -rf ./*.jar
+fi
+
+if [ $n == 4 ]; then
+	rm -rf ./docs/docs.pdf
 fi
 
 echo ""
